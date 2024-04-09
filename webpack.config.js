648d7b8a3380db7dbb102c2a011143ff37442c5d
core-ryan <ryanjr0620@yahoo.com>
2024-04-08 22:44:20 -0600

    - Html Webpack Plugin initialization - Added webpack.config.js     - Build Options     - HTML Plugin config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { Template } = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/index.html",
+})],
+};
